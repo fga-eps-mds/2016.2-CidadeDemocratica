613df799b036f93a78d0c347f12568a6a922c2ce
andreanmasiro <andreanmasiro@live.com>
2016-09-08 19:48:10 -0300

    UC01 - Base User class done (attributes, constructor, getters)

--- CidadeDemocratica/app/src/main/java/com/mdsgpp/cidadedemocratica/model/User.java
+++ CidadeDemocratica/app/src/main/java/com/mdsgpp/cidadedemocratica/model/User.java
@@ -1,8 +1,49 @@
 package com.mdsgpp.cidadedemocratica.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by andreanmasiro on 9/8/16.
  */
 public class User {
 
+    private String name = "";
+    private int proposalCount = 0;
+    private String location = "";
+    private String pictureURL = "";
+    private ArrayList<Proposal> proposals = new ArrayList<Proposal>();
+    private ArrayList<Tag> mostUsedTags = new ArrayList<Tag>();
+
+    public User(String name, int proposalCount, String location, String pictureURL, ArrayList<Proposal> proposals, ArrayList<Tag> mostUsedTags) {
+        this.name = name;
+        this.proposalCount = proposalCount;
+        this.location = location;
+        this.pictureURL = pictureURL;
+        this.proposals = proposals;
+        this.mostUsedTags = mostUsedTags;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getProposalCount() {
+        return this.proposalCount;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public String getPictureURL() {
+        return this.pictureURL;
+    }
+
+    public ArrayList<Proposal> getProposals() {
+        return this.proposals;
+    }
+
+    public ArrayList<Tag> getMostUsedTags() {
+        return this.mostUsedTags;
+    }
 }
