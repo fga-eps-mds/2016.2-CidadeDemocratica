50d39f77b4d7d4c4ee4f0540264ef40a4370a44f
andreanmasiro <andreanmasiro@live.com>
2016-09-08 20:27:44 -0300

    UC01 - ReportGenerator added

---
+++ CidadeDemocratica/app/src/main/java/com/mdsgpp/cidadedemocratica/controller/ReportGenerator.java
@@ -0,0 +1,15 @@
+package com.mdsgpp.cidadedemocratica.controller;
+
+import com.mdsgpp.cidadedemocratica.model.Report;
+
+/**
+ * Created by andreanmasiro on 9/8/16.
+ */
+public class ReportGenerator {
+
+    private Report report;
+
+    void generateReport() {
+        
+    }
+}
