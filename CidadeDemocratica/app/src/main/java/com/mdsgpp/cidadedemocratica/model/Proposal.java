34d5291b81f71d73293691879e80b34b9f5f3490
andreanmasiro <andreanmasiro@live.com>
2016-09-08 19:53:18 -0300

    UC01 - Base Proposal class done (attributes, constructor, getters)

--- CidadeDemocratica/app/src/main/java/com/mdsgpp/cidadedemocratica/model/Proposal.java
+++ CidadeDemocratica/app/src/main/java/com/mdsgpp/cidadedemocratica/model/Proposal.java
@@ -1,8 +1,31 @@
 package com.mdsgpp.cidadedemocratica.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by andreanmasiro on 9/8/16.
  */
 public class Proposal {
 
+    private String title = "";
+    private String content = "";
+    private ArrayList<Tag> tags = new ArrayList<Tag>();
+
+    public Proposal(String title, String content, ArrayList<Tag> tags) {
+        this.title = title;
+        this.content = content;
+        this.tags = tags;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public ArrayList<Tag> getTags() {
+        return this.tags;
+    }
 }
