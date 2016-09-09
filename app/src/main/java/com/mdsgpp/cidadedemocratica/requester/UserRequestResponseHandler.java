401442e802f8f16d2d55180ec60cb51859df714b
andreanmasiro <andreanmasiro@live.com>
2016-09-09 14:20:33 -0300

    UC01 - UserRequestResponseHandler is now JsonHttpResponseHandler subclass

--- app/src/main/java/com/mdsgpp/cidadedemocratica/requester/UserRequestResponseHandler.java
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/requester/UserRequestResponseHandler.java
@@ -1,23 +1,22 @@
 package com.mdsgpp.cidadedemocratica.requester;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-
+import com.loopj.android.http.JsonHttpResponseHandler;
+import org.json.JSONArray;
 import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by andreanmasiro on 9/8/16.
  */
-public class UserRequestResponseHandler extends AsyncHttpResponseHandler {
+public class UserRequestResponseHandler extends JsonHttpResponseHandler {
 
 
     @Override
-    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-
+    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+        super.onSuccess(statusCode, headers, response);
     }
 
     @Override
-    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
+    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+        super.onFailure(statusCode, headers, throwable, errorResponse);
     }
 }
