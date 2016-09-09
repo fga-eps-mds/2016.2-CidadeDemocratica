5b95b63a05bb3ff3d89eeea75baa963c90debaa7
andreanmasiro <andreanmasiro@live.com>
2016-09-09 20:43:57 -0300

    UC01 - Added ProposalRequestResponseHandler class

---
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/requester/ProposalRequestResponseHandler.java
@@ -0,0 +1,23 @@
+package com.mdsgpp.cidadedemocratica.requester;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+
+import cz.msebera.android.httpclient.Header;
+
+/**
+ * Created by andreanmasiro on 9/9/16.
+ */
+public class ProposalRequestResponseHandler extends JsonHttpResponseHandler {
+
+    @Override
+    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+        super.onSuccess(statusCode, headers, response);
+    }
+
+    @Override
+    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+        super.onFailure(statusCode, headers, throwable, errorResponse);
+    }
+}
