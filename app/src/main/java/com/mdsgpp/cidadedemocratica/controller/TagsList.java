f6ef08b8a50a528b5278fe8687bdeb16abc96dbf
Guilherme <guihermefuturo@hotmail.com>
2016-09-13 12:13:55 -0300

    UC02 - Displaying tags on activity_tags_list

--- app/src/main/java/com/mdsgpp/cidadedemocratica/controller/TagsList.java
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/controller/TagsList.java
@@ -2,6 +2,8 @@ package com.mdsgpp.cidadedemocratica.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.mdsgpp.cidadedemocratica.R;
 import com.mdsgpp.cidadedemocratica.model.Tag;
@@ -17,6 +19,12 @@ public class TagsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tags_list);
+
+        ListView tagsListView = (ListView) findViewById(R.id.tags_list);
+
+        ArrayList<Tag> tagsList = getTagsList();
+        ArrayAdapter<Tag> tagsAdapter = new ArrayAdapter<Tag>(this,android.R.layout.simple_list_item_1, tagsList);
+        tagsListView.setAdapter(tagsAdapter);
     }
 
     public final static void pullTagData() {
