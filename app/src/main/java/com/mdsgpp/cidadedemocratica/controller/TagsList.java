6693ecfc02ec74c7c75864bc2ed1486c3b6bfb06
Guilherme <guilhermefuturo@hotmail.com>
2016-09-13 12:10:02 -0300

    UC02 - catching tags from DataContainer

--- app/src/main/java/com/mdsgpp/cidadedemocratica/controller/TagsList.java
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/controller/TagsList.java
@@ -4,8 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.mdsgpp.cidadedemocratica.R;
+import com.mdsgpp.cidadedemocratica.model.Tag;
+import com.mdsgpp.cidadedemocratica.persistence.DataContainer;
 import com.mdsgpp.cidadedemocratica.requester.Requester;
 import com.mdsgpp.cidadedemocratica.requester.TagRequestResponseHandler;
+
+import java.util.ArrayList;
 
 public class TagsList extends AppCompatActivity {
 
@@ -19,4 +23,10 @@ public class TagsList extends AppCompatActivity {
         Requester requester = new Requester("https://cidadedemocratica.herokuapp.com/data/tags", new TagRequestResponseHandler());
         requester.request(Requester.RequestType.GET);
         requester = null;
+    }
+
+    private ArrayList<Tag> getTagsList() {
+        DataContainer dataContainer = DataContainer.getInstance();
+        return dataContainer.getTags();
+    }
 }
