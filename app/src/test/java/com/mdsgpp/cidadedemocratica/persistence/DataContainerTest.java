9195042323a615f707d1541c5cfb555838cd72d4
andreanmasiro <andreanmasiro@live.com>
2016-09-13 17:26:17 -0300

    UC01 - DataContainerTest testing add Users and Proposals, testing DataUpdateListener

--- app/src/test/java/com/mdsgpp/cidadedemocratica/persistence/DataContainerTest.java
+++ app/src/test/java/com/mdsgpp/cidadedemocratica/persistence/DataContainerTest.java
@@ -1,9 +1,10 @@
 package com.mdsgpp.cidadedemocratica.persistence;
 
-import android.provider.ContactsContract;
 import android.test.AndroidTestCase;
 
+import com.mdsgpp.cidadedemocratica.model.Proposal;
 import com.mdsgpp.cidadedemocratica.model.Tag;
+import com.mdsgpp.cidadedemocratica.model.User;
 
 import org.junit.Test;
 
@@ -39,5 +40,87 @@ public class DataContainerTest extends AndroidTestCase {
         dataContainer.addTags(tags);
 
         assertTrue(DataContainer.getInstance().getTags().containsAll(tags));
+    }
+
+    @Test
+    public void testAddProposal() {
+        Proposal proposal = new Proposal("Title", "Content", null);
+
+        dataContainer.addProposal(proposal);
+
+        assertTrue(DataContainer.getInstance().getProposals().contains(proposal));
+
+        ArrayList<Proposal> proposals = new ArrayList<Proposal>();
+
+        for (int i = 0; i < 10; i++) {
+            Proposal propn = new Proposal("Title" + i, "Content", null);
+            proposals.add(propn);
+        }
+
+        dataContainer.addProposals(proposals);
+
+        assertTrue(DataContainer.getInstance().getProposals().containsAll(proposals));
+    }
+
+    @Test
+    public void testAddUser() {
+        User user = new User("Name", 0, "Location", "com.url", null, null);
+
+        dataContainer.addUser(user);
+
+        assertTrue(DataContainer.getInstance().getUsers().contains(user));
+
+        ArrayList<User> users = new ArrayList<User>();
+
+        for (int i = 0; i < 10; i++) {
+            User usern = new User("Name" + i, 0, "Location", "com.url", null, null);
+            users.add(usern);
+        }
+
+        dataContainer.addUsers(users);
+
+        assertTrue(DataContainer.getInstance().getUsers().containsAll(users));
+    }
+
+    @Test
+    public void testUpdateListener() {
+
+        final boolean[] tagsUpdated = {false};
+        final boolean[] proposalsUpdated = {false};
+        final boolean[] usersUpdated = {false};
+
+        dataContainer.setDataUpdateListener(new DataUpdateListener() {
+            @Override
+            public void tagsUpdated() {
+                tagsUpdated[0] = true;
+            }
+
+            @Override
+            public void proposalsUpdated() {
+                proposalsUpdated[0] = true;
+            }
+
+            @Override
+            public void usersUpdated() {
+                usersUpdated[0] = true;
+            }
+        });
+
+        Tag tag = new Tag("Name", 0);
+        dataContainer.addTag(tag);
+
+        assertTrue(tagsUpdated[0]);
+
+
+        Proposal proposal = new Proposal("Proposal", "Content", null);
+        dataContainer.addProposal(proposal);
+
+        assertTrue(proposalsUpdated[0]);
+
+
+        User user = new User("User", 0, "Location", "com.url", null, null);
+        dataContainer.addUser(user);
+
+        assertTrue(usersUpdated[0]);
     }
 }
