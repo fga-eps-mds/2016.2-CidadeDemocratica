5a7fa0f8b5d7825913b0bbd127b2f800fc56a7d0
Guilherme <guihermefuturo@hotmail.com>
2016-09-13 11:57:17 -0300

    UC02 - creating TagsList class

---
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/controller/TagsList.java
@@ -0,0 +1,15 @@
+package com.mdsgpp.cidadedemocratica.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mdsgpp.cidadedemocratica.R;
+
+public class TagsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tags_list);
+    }
+}
