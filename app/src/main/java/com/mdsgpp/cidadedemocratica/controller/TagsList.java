d5742a2bb4c535746bf855c096b298e1154ce469
Guilherme <guihermefuturo@hotmail.com>
2016-09-13 12:04:12 -0300

    UC02 - creating ListView layout file and populating DataContainer with tags

--- app/src/main/java/com/mdsgpp/cidadedemocratica/controller/TagsList.java
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/controller/TagsList.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.mdsgpp.cidadedemocratica.R;
+import com.mdsgpp.cidadedemocratica.requester.Requester;
+import com.mdsgpp.cidadedemocratica.requester.TagRequestResponseHandler;
 
 public class TagsList extends AppCompatActivity {
 
@@ -12,4 +14,9 @@ public class TagsList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tags_list);
     }
+
+    public final static void pullTagData() {
+        Requester requester = new Requester("https://cidadedemocratica.herokuapp.com/data/tags", new TagRequestResponseHandler());
+        requester.request(Requester.RequestType.GET);
+        requester = null;
 }
