2a49c0a91418d4db7c1546039aabcc4669ef23b0
thiagotmb <tmb0710@gmail.com>
2016-09-13 02:51:19 -0300

    Adding permission to run scripts in circle ci

--- CheckCoverageGoal.sh
+++ CheckCoverageGoal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-coverageExpected="0.0"
+coverageExpected="30.0"
 
 coverageResult=$(grep -oE '[0-9.]+%' ./app/build/reports/jacoco/jacocoTestFreeDebugUnitTestReport/html/index.html | head -n1)
 coverageResult="${coverageResult//%}"
