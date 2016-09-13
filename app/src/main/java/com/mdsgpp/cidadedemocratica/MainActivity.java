86e73f902286060615f0b3aaf9a8dd3aa0ca614c
Guilherme <guilhermefuturo@hotmail.com>
2016-09-13 12:24:28 -0300

    UC02 - Adding functionality to show the list of tags in the Tag button

--- app/src/main/java/com/mdsgpp/cidadedemocratica/MainActivity.java
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/MainActivity.java
@@ -1,7 +1,11 @@
 package com.mdsgpp.cidadedemocratica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.mdsgpp.cidadedemocratica.controller.TagsList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showTagsList(View view){
+        Intent tagsIntent = new Intent(this,TagsList.class);
+        startActivity(tagsIntent);
     }
 }
