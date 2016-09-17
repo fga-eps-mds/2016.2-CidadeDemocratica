109c46f64dcbc79ed6b9a18a0b3b6ec03b270937
Guilherme <guilhermefuturo@hotmail.com>
2016-09-17 19:49:02 -0300

    UC02 - Creating SearchTags Activity

---
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/controller/SearchTags.java
@@ -0,0 +1,17 @@
+package com.mdsgpp.cidadedemocratica.controller;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mdsgpp.cidadedemocratica.R;
+
+public class SearchTags extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchable);
+
+    }
+}
