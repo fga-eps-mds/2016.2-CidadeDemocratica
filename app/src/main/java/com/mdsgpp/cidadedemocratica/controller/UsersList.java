de5918a7c92bac577b0a2498498e0fda002d1347
Guilherme <guilhermefuturo@hotmail.com>
2016-09-20 15:58:57 -0300

    Finishing UsersList and adding a ListView to users_list layout file

--- app/src/main/java/com/mdsgpp/cidadedemocratica/controller/UsersList.java
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/controller/UsersList.java
@@ -3,6 +3,7 @@ package com.mdsgpp.cidadedemocratica.controller;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.mdsgpp.cidadedemocratica.R;
@@ -12,6 +13,7 @@ import com.mdsgpp.cidadedemocratica.requester.Requester;
 import com.mdsgpp.cidadedemocratica.requester.UserRequestResponseHandler;
 
 import java.util.ArrayList;
+import java.util.jar.Attributes;
 
 public class UsersList extends AppCompatActivity {
 
@@ -20,8 +22,11 @@ public class UsersList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_list);
 
-        ListView usersListView = (ListView) findViewById(R.id.usersList);
+        ListView usersListView = (ListView) findViewById(R.id.userList);
 
+        ArrayList<User> usersList = getUsersList();
+        ArrayAdapter<User> userAdapter = new ArrayAdapter<User>(this, android.R.layout.simple_list_item_1,usersList);
+        usersListView.setAdapter(userAdapter);
     }
 
     public final static void pullUsersData(){
