b2d4adac883497f9517749d914e26be7101d9dfc
Gabriel Ávila Rangel <gabriel_avila_rangel@hotmail.com>
2016-09-20 21:05:22 -0300

    Fixed bugs on ProposalListAdapter and ProposalListRow

---
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/controller/ProposalListAdapter.java
@@ -0,0 +1,7 @@
+package com.mdsgpp.cidadedemocratica.controller;
+
+/**
+ * Created by gabriel on 20/09/16.
+ */
+public class ProposalListAdapter {
+}
---
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/model/ProposalListRow.java
@@ -0,0 +1,7 @@
+package com.mdsgpp.cidadedemocratica.model;
+
+/**
+ * Created by gabriel on 20/09/16.
+ */
+public class ProposalListRow {
+}
