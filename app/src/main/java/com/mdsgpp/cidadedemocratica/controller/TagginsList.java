eb8b4f969ba85928582b6e93781a7bd038bc39e2
luisresende <luisfrvilela@gmail.com>
2016-10-13 10:09:00 -0300

    Implement share action

--- app/src/main/java/com/mdsgpp/cidadedemocratica/controller/TagginsList.java
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/controller/TagginsList.java
@@ -1,7 +1,10 @@
 package com.mdsgpp.cidadedemocratica.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,12 +16,13 @@ import com.mdsgpp.cidadedemocratica.persistence.DataContainer;
 
 import java.util.ArrayList;
 
-public class TagginsList extends AppCompatActivity {
+public class TagginsList extends AppCompatActivity implements View.OnClickListener{
 
     ListView tagginsListView;
     TextView proposalTitleTextView;
     TextView proposalDescripitionTextView;
     TextView relevanceTextView;
+    Button shareButton;
 
     Proposal proposal;
 
@@ -51,6 +55,9 @@ public class TagginsList extends AppCompatActivity {
         TagListAdapter tagginsAdapter = new TagListAdapter(this, tags);
 
         tagginsListView.setAdapter(tagginsAdapter);
+
+        shareButton = (Button) findViewById(R.id.shareButton);
+        shareButton.setOnClickListener(this);
     }
 
     private ArrayList<Tag> getTagsList() {
@@ -61,4 +68,28 @@ public class TagginsList extends AppCompatActivity {
         }
     }
 
+    private void shareProposal(){
+
+        String subjectShare = getString(R.string.title_sharing) + proposal.getTitle();
+        String contentShare = subjectShare + getString(R.string.title_sharing_description) + proposal.getContent() + getString(R.string.link_site_cidade_democratica);
+
+        Intent intentShare =new Intent(android.content.Intent.ACTION_SEND);
+
+        intentShare.setType("text/plain");
+
+        intentShare.putExtra(android.content.Intent.EXTRA_SUBJECT,subjectShare);
+        intentShare.putExtra(android.content.Intent.EXTRA_TEXT, contentShare);
+
+        startActivity(Intent.createChooser(intentShare,getString(R.string.name_action_share)));
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.shareButton:
+                shareProposal();
+                break;
+        }
+
+    }
 }
