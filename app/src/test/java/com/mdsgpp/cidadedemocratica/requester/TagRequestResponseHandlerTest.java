6514e71d6e8d2847a1a860a8b71883a7bbf6170f
andreanmasiro <andreanmasiro@live.com>
2016-11-04 23:19:21 -0200

    tag request handler on success test added

--- app/src/test/java/com/mdsgpp/cidadedemocratica/requester/TagRequestResponseHandlerTest.java
+++ app/src/test/java/com/mdsgpp/cidadedemocratica/requester/TagRequestResponseHandlerTest.java
@@ -4,29 +4,54 @@ import android.test.AndroidTestCase;
 
 import com.mdsgpp.cidadedemocratica.model.Tag;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.junit.Test;
+
 import java.util.ArrayList;
 
 /**
  * Created by andreanmasiro on 04/11/16.
  */
 
-public class TagRequestResponseHandlerTest extends AndroidTestCase {
+public class TagRequestResponseHandlerTest extends AndroidTestCase implements RequestUpdateListener {
 
-    TagRequestResponseHandler handler;
+    TagRequestResponseHandler handler = new TagRequestResponseHandler();
     ArrayList<Tag> response = null;
     Tag tag;
-    int id = 2572;
-    int userId = 6418;
+    int id = 1;
+    String tagName = "internet";
     String errorMessage = null;
 
     @Override
     protected void setUp() {
-        handler = new TagRequestResponseHandler();
+        handler.setRequestUpdateListener(this);
     }
 
     @Override
     protected void tearDown() throws Exception {
         response = null;
         errorMessage = null;
+    }
+
+    @Test
+    public void testOnSuccess() throws JSONException {
+        JSONArray jsonTag = new JSONArray("[{\"id\":" + id + ",\"name\":\"" + tagName + "\",\"relevancia\":982100}]");
+        handler.onSuccess(200, null, jsonTag);
+
+        assertEquals(id, tag.getId());
+        assertEquals(tagName, tag.getName());
+    }
+
+
+    @Override
+    public void afterSuccess(RequestResponseHandler handler, Object response) {
+        ArrayList<Tag> tags = (ArrayList<Tag>) response;
+        tag = tags.get(0);
+    }
+
+    @Override
+    public void afterError(RequestResponseHandler handler, String message) {
+
     }
 }
