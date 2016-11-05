3d6e0782a1feccca714402be9e9293828479df02
andreanmasiro <andreanmasiro@live.com>
2016-11-04 23:11:37 -0200

    Tag request handler now handles Tag comparisons

--- app/src/main/java/com/mdsgpp/cidadedemocratica/requester/TagRequestResponseHandler.java
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/requester/TagRequestResponseHandler.java
@@ -1,6 +1,5 @@
 package com.mdsgpp.cidadedemocratica.requester;
 
-import com.loopj.android.http.JsonHttpResponseHandler;
 import com.mdsgpp.cidadedemocratica.model.Tag;
 import com.mdsgpp.cidadedemocratica.persistence.DataContainer;
 
@@ -19,7 +18,7 @@ import cz.msebera.android.httpclient.Header;
 /**
  * Created by andreanmasiro on 9/9/16.
  */
-public class TagRequestResponseHandler extends RequestResponseHandler {
+public class TagRequestResponseHandler extends RequestResponseHandler implements Comparator<Tag> {
 
     DataContainer dataContainer = DataContainer.getInstance();
 
@@ -35,7 +34,7 @@ public class TagRequestResponseHandler extends RequestResponseHandler {
     public void onSuccess(int statusCode, Map<String, List<String>> headers, JSONArray response) {
 
         if (statusCode == 200) {
-            ArrayList<Tag> tags = new ArrayList<Tag>();
+            ArrayList<Tag> tags = new ArrayList<>();
             for (int i = 0; i < response.length(); ++i) {
                 try {
                     JSONObject tagJson = response.getJSONObject(i);
@@ -50,12 +49,7 @@ public class TagRequestResponseHandler extends RequestResponseHandler {
                 }
             }
 
-            Collections.sort(tags, new Comparator<Tag>() {
-                @Override
-                public int compare(Tag t1, Tag t2) {
-                    return t1.compareTo(t2);
-                }
-            });
+            Collections.sort(tags, this);
             tags.removeAll(dataContainer.getTags());
             dataContainer.addTags(tags);
             afterSuccess(tags);
@@ -67,5 +61,10 @@ public class TagRequestResponseHandler extends RequestResponseHandler {
     public void onFailure(int statusCode, Map<String, List<String>> headers, JSONArray errorResponse) {
         super.onFailure(statusCode, headers, errorResponse);
         afterError(String.valueOf(statusCode));
+    }
+
+    @Override
+    public int compare(Tag t1, Tag t2) {
+        return t1.compareTo(t2);
     }
 }
