9848a6454c52b1039901999b48a867b33d10f80a
andreanmasiro <andreanmasiro@live.com>
2016-11-13 13:03:33 -0200

    requester post settings

--- app/src/main/java/com/mdsgpp/cidadedemocratica/requester/Requester.java
+++ app/src/main/java/com/mdsgpp/cidadedemocratica/requester/Requester.java
@@ -4,8 +4,10 @@ import android.support.annotation.NonNull;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -21,6 +23,8 @@ import java.util.Map;
  */
 public class Requester {
 
+    private static String userToken = null;
+
     public enum RequestMethod {
         POST, GET, PATCH, DELETE
     }
@@ -32,7 +36,10 @@ public class Requester {
     private Thread requestThread = Thread.currentThread();
     private String url = "";
     private HashMap<String, String> parameters = new HashMap<>();
+    private HashMap<String, String> headers = new HashMap<>();
     private RequestResponseHandler responseHandler;
+
+
 
     public Requester(String url, RequestResponseHandler responseHandler) {
         this.url = url;
@@ -42,15 +49,32 @@ public class Requester {
 
     private void requestSync(RequestMethod method) {
         try {
-            URL url = new URL(getUrlWithParameters());
+
+            URL url = new URL(this.url);
+            if (method == RequestMethod.GET) {
+                url = new URL(getUrlWithParameters());
+            }
 
             HttpURLConnection urlConnection = getHttpURLConnection(url, method);
 
-            urlConnection.setRequestProperty("Authorization", "");
+            if (userToken != null) {
+                headers.put("Authorization", userToken);
+            }
+            for (String key : headers.keySet()) {
+                urlConnection.setRequestProperty(key, headers.get(key));
+            }
+
+
             urlConnection.connect();
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            if (method == RequestMethod.POST) {
+                DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream());
+                wr.writeBytes(getParameters());
+                wr.flush();
+                wr.close();
+            }
 
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
             StringBuilder sb = new StringBuilder();
             String line;
             while ((line = br.readLine()) != null) {
@@ -61,21 +85,32 @@ public class Requester {
             br.close();
 
             String jsonString = sb.toString();
-
-            JSONArray ja = new JSONArray(jsonString);
+            JSONArray ja = null;
+            JSONObject jo = null;
+            try {
+                ja = new JSONArray(jsonString);
+            } catch (JSONException e) {
+                try {
+                    jo = new JSONObject(jsonString);
+                } catch (JSONException e1) {
+                    e1.printStackTrace();
+                }
+            }
 
             int responseCode = urlConnection.getResponseCode();
 
             Map<String, List<String>> headers = urlConnection.getHeaderFields();
 
             if (responseCode >= 200 && responseCode - 200 < 100) {
-                responseHandler.onSuccess(responseCode, headers, ja);
+                if (ja != null) {
+                    responseHandler.onSuccess(responseCode, headers, ja);
+                } else if (jo != null) {
+                    responseHandler.onSuccess(responseCode, headers, jo);
+                }
             } else {
                 responseHandler.onFailure(responseCode, headers, ja);
             }
         } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
             e.printStackTrace();
         } catch (UnknownHostException e) {
             responseHandler.onFailure(500, null, null);
@@ -111,7 +146,12 @@ public class Requester {
         urlConnection.setReadTimeout(10000 /* milliseconds */);
         urlConnection.setConnectTimeout(15000 /* milliseconds */);
 
-        urlConnection.setDoOutput(false);
+        if (method == RequestMethod.GET) {
+            urlConnection.setDoOutput(false);
+        } else {
+            urlConnection.setDoOutput(true);
+        }
+
 
         return urlConnection;
     }
@@ -155,15 +195,44 @@ public class Requester {
         String purl = this.url;
 
         String prefix = "?";
-        for (String key : parameters.keySet()) {
-            purl += prefix + key + "=" + parameters.get(key);
-            prefix = "&";
+        String parameters = getParameters();
+        if (parameters.length() > 0) {
+            purl += prefix + parameters;
         }
 
         return purl;
     }
 
+    public String getParameters() {
+
+        String urlParameters = "";
+        String prefix = "";
+
+        for (String key : parameters.keySet()) {
+            urlParameters += prefix + key + "=" + parameters.get(key);
+            prefix = "&";
+        }
+
+        return urlParameters;
+    }
+
     public void setParameter(String key, String parameter) {
         parameters.put(key, parameter);
+    }
+
+    public HashMap<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeader(String key, String value) {
+        headers.put(key, value);
+    }
+
+    public static String getUserToken() {
+        return userToken;
+    }
+
+    public static void setUserToken(String userToken) {
+        Requester.userToken = userToken;
     }
 }
